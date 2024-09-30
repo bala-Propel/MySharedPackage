8a90ca004282bb5f2cefd9579c55f5b7b4d6c625
bala-Propel <balamurugan@propelsoft.in>
2024-09-30 11:11:10 +0530

    add

---
+++ src/ADD/calculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Prope\MySharedPackage\ADD;
+
+class calculator
+{
+    public function add()
+    {
+        $a = 1;
+        $b = 2;
+        return $a + $b;
+    }
+}
--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -6,13 +6,6 @@ use Prope\MySharedPackage\Interfaces\RepositoryInterface;
 
 class Repository implements RepositoryInterface
 {
-    protected $model;
-
-    public function __construct($model)
-    {
-        $this->model = $model; // Inject the model
-    }
-
     public function getData($id)
     {
        return "connected";
